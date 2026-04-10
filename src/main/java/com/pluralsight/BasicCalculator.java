f87973e5d28b4bcdb99a218dada1aef97ec4e2fc
Shah Ali Omor <omoranam@gmail.com>
2026-04-10 10:50:04 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Ask for numbers (float)
+        System.out.print("Enter first number: ");
+        float num1 = input.nextFloat();
+
+        System.out.print("Enter second number: ");
+        float num2 = input.nextFloat();
+
+        // Ask for operation
+        System.out.print("Choose operation (add, subtract, multiply, divide): ");
+        String operation = input.next();
+
+        // ALWAYS multiply (as instructed)
+        float result = num1 * num2;
+
+        // Print result
+        System.out.println("Result is: " + result);
+    }
+}
